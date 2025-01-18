abf4158f4ee4c9e683995953b3663b92aaa55fa1
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2025-01-19 00:28:17 +0530

    feat: Add 404 Not Found page component for improved user experience

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const notFoundPage = () => {
+  return (
+    <div className="flex min-h-[80vh] w-auto justify-center items-center text-2xl md:text-3xl">
+      404 | Page not found
+    </div>
+  )
+}
+
+export default notFoundPage
